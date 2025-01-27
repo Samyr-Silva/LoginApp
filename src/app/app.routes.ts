ee8aad1b62be3e4763144eae9e140b4418c91a2f
samyr silva <ssamyrsilva.santana@gmail.com>
2025-01-27 16:18:29 +0000

    sign up page done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { DefaultLoginPageComponent } from './components/default-login-page/default-login-page.component';
 import { LoginComponent } from './pages/login/login.component';
+import { SignupComponent } from './pages/signup/signup.component';
 export const routes: Routes = [
     {
         path: "login",
         component: LoginComponent
+    },
+    {
+        path: "signup",
+        component: SignupComponent
     }
 ];
---
+++ src/app/pages/signup/signup.component.ts
@@ -0,0 +1,47 @@
+import { Component } from '@angular/core';
+import { DefaultLoginPageComponent } from '../../components/default-login-page/default-login-page.component';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { PrimaryInputComponent } from '../../components/primary-input/primary-input.component';
+import { Router } from '@angular/router';
+import { LoginService } from '../../services/login.service';
+ 
+interface SignupForm {
+  name: FormControl;
+  email: FormControl;
+  password: FormControl;
+  passwordConfirm: FormControl;
+}
+@Component({
+  selector: 'app-signup',
+  imports: [DefaultLoginPageComponent, ReactiveFormsModule, PrimaryInputComponent],
+  providers: [
+    LoginService,
+  ], 
+  templateUrl: './signup.component.html',
+  styleUrl: './signup.component.css'
+})
+export class SignupComponent {
+  signupForm!: FormGroup<SignupForm>;
+
+  constructor(
+    private router: Router,
+    private loginService: LoginService,
+  ) {
+    this.signupForm = new FormGroup({
+      name: new FormControl("", [Validators.required,  Validators.minLength(3)]),
+      email: new FormControl("", [Validators.required, Validators.email]),
+      password: new FormControl("", [Validators.required, Validators.minLength(6)]),
+      passwordConfirm: new FormControl("", [Validators.required, Validators.minLength(6)])
+    })
+   }
+
+   submit(){
+    this.loginService.login(this.signupForm.value.email, this.signupForm.value.password).subscribe({
+      next: () => console.log("sucesso"),
+      error: () => console.error("error")
+    });
+   }
+   navigate (){
+    this.router.navigate(["login"]);
+   }
+} 
